e68c8ff8f6600958eb1f63288d9f91a8bd4d27bb
enonglosker <ENONGLOSKER@gmail.com>
2024-07-28 06:06:25 +0700

    update-dashboard

--- api_pemesanan_cafe/settings.py
+++ api_pemesanan_cafe/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -174,3 +175,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+JAZZMIN_SETTINGS = {
+    "site_title": "CAFE COCODAY",
+    "site_header": "Cafe Cocoday",
+    "site_brand": "CAFE COCODAY",
+    "site_logo": "../static/img/logos.png",
+    "welcome_sign": "Selamat Datang di Cafe Cocoday",
+    # Copyright on the footer
+    "copyright": "elq132",
+}
