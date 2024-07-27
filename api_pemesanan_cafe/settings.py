e254cf2f57cfd59103127e972b278b180c322423
enonglosker <ENONGLOSKER@gmail.com>
2024-07-28 05:13:37 +0700

    fix-database

--- api_pemesanan_cafe/settings.py
+++ api_pemesanan_cafe/settings.py
@@ -104,18 +104,18 @@ WSGI_APPLICATION = 'api_pemesanan_cafe.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 
     # database dengan mysql
-    'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'db_cafe',
-            'USER': 'root',
-            'PASSWORD': '',
-        }
+    # 'default': {
+    #         'ENGINE': 'django.db.backends.mysql',
+    #         'NAME': 'db_cafe',
+    #         'USER': 'root',
+    #         'PASSWORD': '',
+    #     }
 }
 
 
